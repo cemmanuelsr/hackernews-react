2672fd4d5dece77908c6362c4e488c654df58dc5
cemmanuelsr <caioemmanuelsr@gmail.com>
2022-04-18 00:30:26 -0300

    implementing realtime updates with graphql subscriptions and websockets

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import Link from './Link';
 import { useQuery, gql } from '@apollo/client';
+
+import Link from './Link';
+
 
 export const FEED_QUERY = gql`
   {
@@ -22,9 +24,53 @@ export const FEED_QUERY = gql`
   }
 `
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      voters {
+        id
+      }
+    }
+  }
+`;
+
 const LinkList = () => {
 
-  const { data } = useQuery(FEED_QUERY);
+  const {
+    data,
+    loading,
+    error,
+    subscribeToMore
+  } = useQuery(FEED_QUERY);
+
+
+  subscribeToMore({
+    document: NEW_LINKS_SUBSCRIPTION,
+    updateQuery: (prev, { subscriptionData }) => {
+      if (!subscriptionData.data) return prev;
+      const newLink = subscriptionData.data.newLink;
+      const exists = prev.feed.links.find(
+        ({ id }) => id === newLink.id
+      );
+      if (exists) return prev;
+
+      return Object.assign({}, prev, {
+        feed: {
+          links: [newLink, ...prev.feed.links],
+          count: prev.feed.links.length + 1,
+          __typename: prev.feed.__typename
+        }
+      });
+    }
+  });
 
   return (
     <div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache, split } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { setContext } from '@apollo/client/link/context';
 
 import App from './components/App';
 import './styles/index.css';
 import { AUTH_TOKEN } from './constants';
 
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:5000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:5000'
@@ -23,8 +35,20 @@ const authLink = setContext((_, { headers }) => {
   }
 });
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return (
+      kind === 'OperationDefinition' &&
+      operation === 'subscription'
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
 
