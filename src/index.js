1467891ec7b0cd92b6b0d9829fa5c5ccd5e0bde1
cemmanuelsr <caioemmanuelsr@gmail.com>
2022-04-17 23:36:57 -0300

    adding post link mutation and getting jwt token from localStorage

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
 import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:5000'
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? token : "",
+    }
+  }
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
